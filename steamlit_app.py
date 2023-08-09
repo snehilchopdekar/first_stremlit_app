b7a407e45366d268381353ae7cbae1d609b7d711
snehilchopdekar <70432860+snehilchopdekar@users.noreply.github.com>
2023-08-09 13:18:09 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -21,6 +21,8 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
